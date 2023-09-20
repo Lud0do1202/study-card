3b76b8229ec269ecd32e48877865612e1bfb9634
Lud0do1202 <ludo.traina@gmail.com>
2023-09-20 11:58:21 +0200

    fit-content textarea in edit-card component

--- src/app/components/card/edit-card/edit-card.component.ts
+++ src/app/components/card/edit-card/edit-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChildren } from '@angular/core';
 import { Card } from 'src/app/interfaces/card';
 import { Topic } from 'src/app/interfaces/topic';
 import { RouterService } from 'src/app/services/router.service';
@@ -8,12 +8,26 @@ import { RouterService } from 'src/app/services/router.service';
   templateUrl: './edit-card.component.html',
   styleUrls: ['./edit-card.component.scss'],
 })
-export class EditCardComponent {
+export class EditCardComponent implements AfterViewInit {
+  /* ---------------------------------- Va ---------------------------------- */
   @Input() topic!: Topic;
   @Input() card!: Card;
 
+  @ViewChildren('cardInfos') cardInfos!: ElementRef[];
+
+  /* ------------------------------ Constructor ----------------------------- */
   constructor(private router: RouterService) {}
 
+  /* ---------------------------- After View Init --------------------------- */
+  ngAfterViewInit(): void {
+    for (let info of this.cardInfos) {
+      const infoElement = info.nativeElement as HTMLTextAreaElement;
+      infoElement.style.height = 'auto';
+      infoElement.style.height = infoElement.scrollHeight + 'px';
+    }
+  }
+
+  /* --------------------------------- Edit --------------------------------- */
   editCard(): void {
     this.router.editCardPage(this.topic, this.card);
   }
